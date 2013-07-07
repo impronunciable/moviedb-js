6548037d586cffb2ecda58d2263ddcc4ef05249e
Dan Zajdband <dan.zajdband@gmail.com>
2013-07-07 10:46:16 -0300

    added image functionality

--- index.js
+++ index.js
@@ -41,12 +41,36 @@ MovieDB.prototype.get = function(endpoint, opts, fn) {
   var self = this;
   opts.api_key = this.api_key;
 
+  endpoint = endpoint.replace('{id}', opts.id);
+
   request.get(API_BASE_URL + endpoint)
     .query(opts)
     .end(function(res){
       fn && fn.call(self, res.error, res.body);
     });
 };
+
+/*
+ * POST requests
+ *
+ * @param {String} endpoint
+ * @param {Object} request body
+ * @param {Function} callback
+ */
+
+MovieDB.prototype.post = function(endpoint, opts, fn) {
+  var self = this;
+
+  endpoint = endpoint.replace('{id}', opts.id);
+
+  request.post(API_BASE_URL + endpoint)
+    .query({api_key: this.api_key, session_id: this.session_id})
+    .send(opts)
+    .end(function(res){
+      fn && fn.call(self, res.error, res.body);
+    });
+};
+
 
 /*
  * Auto configuration
@@ -59,6 +83,36 @@ MovieDB.prototype.configure = function(fn) {
 
     fn && fn.call(this);
   });
+};
+
+/*
+ * Get full image url (first need to query "configure")
+ *
+ * @param {String} Image path
+ * @param {String} Image size (defaults to original)
+ * @return {String} full url
+ */
+
+MovieDB.prototype.getImageURL = function(path, size) {
+  if(!this.config) {
+    throw new Error("Please fetch config first via MovieDB#configure() method.");
+  }
+  return this.secure_base_url + (size || "original") + path;
+};
+
+/*
+ * Get config image sizes (first need to query "configure")
+ *
+ * @param {String} poster | backdrop | profile | logo
+ * @return {Array} Image sizes
+ */
+
+MovieDB.prototype.getImageSizes = function(type) {
+  if(this.config) {
+    return this.config[type + "_sizes"];
+  } else {
+    throw new Error("Please fetch config first via MovieDB#configure() method.");
+  }
 };
 
 /*
